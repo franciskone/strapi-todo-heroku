b0e9473ff80b89d96123db8e5065a06ad99cf018
franciskone <francesco.cappelli.developer@gmail.com>
2021-02-04 19:46:29 +0100

    initial strapi boilerplate

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '67d6c0c09696036e0567903e0ace94bc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '765d6cf3-8634-4481-a358-82184ff1f521'
+};
